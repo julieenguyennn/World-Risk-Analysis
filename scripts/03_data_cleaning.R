0c5b86901e73f33c9ac43376736ca687c34da469
Julie Nguyen <hongan.nguyen@mail.utoronto.ca>
2023-04-19 10:29:31 -0400

    update files

--- scripts/03_data_cleaning.R
+++ scripts/03_data_cleaning.R
@@ -30,5 +30,28 @@ cleaned_data <- cleaned_data %>% mutate(global_region = case_when(
   global_region == 15 ~ "Australia and New Zealand"
 ))
 
+# Rename labels
+cleaned_data <- cleaned_data %>%
+  rename("internet_use" = l26, "online_bullying" = l27a, "false_info" = l27b, "fraud" = l27c)
+
+# Filter and remove NA values
+cleaned_data <- cleaned_data %>% 
+  filter(internet_use == 1) %>% 
+  filter(online_bullying == 1 | online_bullying == 2) %>% 
+  filter(false_info == 1 | false_info == 2) %>% 
+  filter(fraud == 1 | fraud == 2) %>% 
+  filter(!education == 4 | !education == 5) %>% 
+  filter(!urbanicity == 9) %>% 
+  filter(!children_in_household == 9)
+
+# Recode values
+cleaned_data$gender <- ifelse(test=cleaned_data$gender == 1, yes= "Male", no = "Female")
+
+cleaned_data$online_bullying <- ifelse(test=cleaned_data$online_bullying == 1, yes="Worried", no="Not worried")
+
+cleaned_data$false_info <- ifelse(test=cleaned_data$false_info == 1, yes="Worried", no="Not worried")
+
+cleaned_data$fraud <- ifelse(test=cleaned_data$fraud == 1, yes="Worried", no="Not worried")
+
 write_csv(cleaned_data, "inputs/cleaned_data.csv")
 
